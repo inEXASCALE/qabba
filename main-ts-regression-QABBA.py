a90c4cb4842581ef4705f7ab2ca5a0541a0b1004
chenxinye <cclcquant@yahoo.com>
2024-07-06 17:45:30 +0200

    add new files

--- main-ts-regression-QABBA.py
+++ main-ts-regression-QABBA.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import json
-from qabba import QABBA
+from software.qabba import QABBA
 import os
 import pickle
 from sklearn import preprocessing
